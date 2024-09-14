0ed31a93b63b28b74be6fc486d862792505ec949
BlackHat <123658198+Mubaraq0@users.noreply.github.com>
2024-09-14 06:48:28 +0100

    Update classicconf.js

--- classicconf.js
+++ classicconf.js
@@ -28,11 +28,11 @@ global.region = 'kenya' // serah
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'DRAGON' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['254746764468'] // ubah aja pake nomor lu
+global.owner = ['2348022444782'] // ubah aja pake nomor lu
 //==========================zetsuboclient=======================//
 global.botname = '𝚻𝚰𝐆𝚵𝚪 𝐑𝚫𝐃𝚰𝚯𝚫𝐂𝚻𝚰𝛁𝚵 𝛁1' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.packname = '𝚻𝚰𝐆𝚵𝚪 𝐑𝚫𝐃𝚰𝚯𝚫𝐂𝚻𝚰𝛁𝚵 𝛁1' // ubah aja ini nama sticker
-global.author = '༻࿇𝚻𝚮𝚵𝚵 𝐃𝚪𝚫𝐆𝚯𝚴❖༺' // ubah aja ini nama sticker
+global.author = 'Muba' // ubah aja ini nama sticker
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sessionName = 'Tigersession'  //Gausah Juga
 global.sp = '⭔' // Gausah Juga
